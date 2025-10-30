16b1a0086ed66a7c1a0f2cbc7a07e4182cb2cb24
FabianDim <63860191+FabianDim@users.noreply.github.com>
2025-10-30 02:02:46 -0400

    Begin intergration with drizzle

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,10 +3,13 @@ import ViteExpress from "vite-express";
 
 const app = express();
 
+import { drizzle } from "drizzle-orm/node-postgres";
+const db = drizzle(process.env.DATABASE_URL!);
+
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+  console.log("Server is listening on port 3000...")
 );
