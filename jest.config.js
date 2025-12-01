86197106c4bcfe54a37c126a8cec6982b3eea857
jessbarretto19 <nbarretto@umass.edu>
2025-12-01 16:47:46 -0500

    jest import + inbox tests + upcoming apps endpoint

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+import { createDefaultPreset } from "ts-jest";
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      useESM: true,                      
+    },
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  }, 
+};
