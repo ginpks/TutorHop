72dc55e454a6398766417d55fe1815bf888d353d
jessbarretto19 <nbarretto@umass.edu>
2025-12-02 10:35:04 -0500

    Landing button + upcoming

--- src/client/pages/landing.tsx
+++ src/client/pages/landing.tsx
@@ -1,4 +1,4 @@
-import { Box, Card } from "@mui/material";
+import { Box, Card, CardActions } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import DefaultBanner from "../components/main_banner/banner.js";
 import InboxCardComponent from "../components/LandingInbox.js";
@@ -6,13 +6,19 @@ import React, { useEffect, useState } from "react";
 import { MailPreviewMessages } from "../../shared/Interfaces/InboxInterfaces.js";
 import { roleLabels, UserRole } from "../../shared/Enums/UserEnums.js";
 import UpcomingAppointment from "../components/UpcomingAppointment.js";
+import PrimaryButton from "../components/primary-button.js";
 import UserSearchBar from "../components/SearchBar.js";
+import { useNavigate } from "react-router-dom";
 
 function Landing() {
   const [messages, setMessages] = React.useState<MailPreviewMessages[]>([]);
   const [userID, setUserID] = React.useState<number>(0);
   const [isTutor, setIsTutor] = React.useState<boolean>(false);
   const [loadingInbox, setLoadingInbox] = React.useState<boolean>(true);
+  const [upcoming, setUpcoming] = React.useState<MailPreviewMessages[]>([])
+  const createReq = () => {
+      useNavigate()("/survey");
+    };
 
   useEffect(() => {
     //set a test user
@@ -36,8 +42,19 @@ function Landing() {
           throw new Error(`HTTP ${res.status}`);
         }
 
+        const res2 = await fetch(`/appointments/${userID}/upcoming?${query.toString()}`);
+
+        if(!res2.ok) {
+          const text = await res2.text();
+          console.error("HTTP error", res2.status, text.slice(0, 300));
+          throw new Error(`HTTP ${res2.status}`);
+        }
+        
         const data = (await res.json()) as MailPreviewMessages[];
+        const apps = (await res2.json()) as MailPreviewMessages[];
+
         setMessages(data);
+        setUpcoming(apps);
         setLoadingInbox(false);
       } catch (err) {
         console.error("Inbox fetch error: ", err);
@@ -86,14 +103,19 @@ function Landing() {
         >
           <UpcomingAppointment
             userType={roleLabels[UserRole?.STUDENT]}
-            appointments={[]}
+            appointments={upcoming}
           />
         </Box>
       </Box>
       {/* <Box>
         <UserSearchBar userType={roleLabels[UserRole?.STUDENT]}></UserSearchBar>
       </Box> */}
+      <Box display={"flex"} justifyContent={"center"}>
+        <PrimaryButton px={20} text="Request New Appointment" onClick={createReq}/>
+      </Box>
+      
     </Box>
+    
   );
 }
 
